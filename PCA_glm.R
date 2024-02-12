ba455444b99d9f20aba9a51b3c465c67958d3e60
Qi Wang <1061673783@qq.com>
2024-02-12 13:00:55 -0800

    update pca method

--- PCA_glm.R
+++ PCA_glm.R
@@ -1,13 +1,5 @@
 rm(list = ls())
 library(ggplot2)
-library(sp)
-library(fields)
-library(mvtnorm)
-library(FRK)
-library(utils)
-library(keras)
-library(reticulate)
-library(tensorflow)
 library(glmnet)
 library(MASS)
 use_condaenv("tf_gpu")
@@ -21,52 +13,11 @@ nsf_wide_car <- merge(nsf_wide, carnegie_2021, by = "UNITID")
 wide_y <- nsf_wide_car[,-c(1:5, ncol(nsf_wide_car))]
 
 dummy_car <- model.matrix(~nsf_wide_car$HD2021.Carnegie.Classification.2021..Graduate.Instructional.Program - 1)
-dummy_school <- model.matrix(~nsf_wide$UNITID - 1)
+# dummy_school <- model.matrix(~nsf_wide$UNITID - 1)
 # dummy_matrix <- cbind(dummy_school, dummy_car)
-dummy_matrix <- dummy_school
+dummy_matrix <- dummy_car
 
 
-#Assume Beta does not change across the schools.
-shared_beta_pred <- matrix(NA, nrow = nrow(nsf_wide), ncol = length(2015:2021))
-for (i in 2015:2021) {
-  print(paste("Now doing year",i))
-  years_before <- i - 1972
-  prev_y <- t(wide_y)[1:years_before,]
-  prev_pc <- prcomp(prev_y)
-  num_pc <- length(which(cumsum(prev_pc$sdev^2)/sum(prev_pc$sdev^2) <= 0.95 ))
-  prev_pc_use <- prev_pc$x[,1:num_pc]
-
-  j = 1
-
-  #Each school has its own slope but shared across the time, loop across the schoools
-  curr_y <- wide_y[j,2:years_before]
-  curr_pc_cov <- prev_pc_use[1:(nrow(prev_pc_use)-1),] # Use the previous year's principal component
-  curr_cov <- dummy_matrix[j,]
-  curr_dummy <- matrix(rep(curr_cov, nrow(curr_pc_cov)), byrow = TRUE, nrow = nrow(curr_pc_cov))
-  curr_x <- cbind(curr_pc_cov, curr_dummy)
-  curr_dat <- cbind(t(curr_y), curr_x)
-  use_dat <- curr_dat
-  
-  for (j in 2:nrow(nsf_wide)) {
-
-    #Each school has its own slope but shared across the time, loop across the schoools
-    curr_y <- wide_y[j,2:years_before]
-    curr_pc_cov <- prev_pc_use[1:(nrow(prev_pc_use)-1),] # Use the previous year's principal component
-    curr_cov <- dummy_matrix[j,]
-    curr_dummy <- matrix(rep(curr_cov, nrow(curr_pc_cov)), byrow = TRUE, nrow = nrow(curr_pc_cov))
-    curr_x <- cbind(curr_pc_cov, curr_dummy)
-    curr_dat <- cbind(t(curr_y), curr_x)
-    use_dat <- rbind(use_dat, curr_dat)
-  }
-  
-  final_dat <- use_dat[-1,]
-  colnames(final_dat) <- c("y",colnames(curr_pc_cov),colnames(dummy_matrix))
-  curr_model <- glm.nb(y~., data = data.frame(final_dat))
-  pred_pc <- matrix( rep(prev_pc_use[nrow(prev_pc_use),],2002), nrow = 2002, byrow = TRUE)
-  pred_cov <- cbind(pred_pc, dummy_matrix)
-  colnames(pred_cov) <- colnames(final_dat)[-1]
-  shared_beta_pred[,i-2014] <- predict(curr_model, data.frame(pred_cov), type = "response")
-}
 
 write.csv( as.data.frame(shared_beta_pred), "D:/77/UCSC/study/Research/temp/NSF_dat/share_beta_pred.csv", row.names = FALSE)
 

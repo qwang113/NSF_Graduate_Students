ae3a1ca1b32cea1fd1f2659af876a471c7e611a7
Qi Wang <1061673783@qq.com>
2024-09-22 15:16:48 -0500

    update

--- 2024Summer/negative_binom.R
+++ 2024Summer/negative_binom.R
@@ -10,18 +10,6 @@ schools <- read.csv(here::here("nsf_final_wide_car.csv"))
 # %>% filter(state%in%c("CA","OH","TX","WI","IL"))
 schoolsM <- as.matrix(schools[,10:59])
 
-pos_sig_xi <- function(sig_xi, xi){
-  ns <- length(xi)
-  loglike <- -log(1+(sig_xi)^2) -  ns * log(sig_xi) -0.5*(sum(xi^2/sig_xi^2))
-  return(loglike)
-}
-
-pos_sig_eta <- function(sig_eta, eta_trunc){
-  n_eta <- length(eta_trunc)
-  loglike <- -log(1+(sig_eta)^2) -  n_eta * log(sig_eta) - 0.5*(sum(eta_trunc^2/sig_eta^2))
-  return(loglike)
-}
-
 
 
 ESN_expansion <- function(Xin, Yin, Xpred, nh=120, nu=0.8, aw=0.1, pw=0.1, au=0.1, pu=0.1, eps = 1){
@@ -49,21 +37,21 @@ school_idx <- model.matrix( ~ factor(UNITID) - 1, data = schools)
 
 # MCMC parameters
 total_samples <- 1000
-burn = 0
-thin = 1
-years_to_pred <- 46:50
-# years = years_to_pred
-#Hyper-parameters
-# sig_eta_inv = 100
+burn = 100
+thin = 2
+years_to_pred = 46:50
+alpha_eta = 0.001
+beta_eta = 0.001
+alpha_xi = 0.001
+beta_xi = 0.001
 eps = 1 # Avoid underflow, avoid log(0)
 
 
 # ESN Parameters
-nh = 120
+nh = 30
 nu = 0.9
-aw = au = 0.1
+aw = au = 0.01
 pw = pu = 0.1
-reps = 100
 ns = length(unique(schools$state))
 N = length(unique(schools$UNITID))
 
@@ -93,7 +81,7 @@ for(years in years_to_pred){
   
   # Posterior sample boxes
   tilde_eta <- matrix(NA, ncol = total_samples, nrow = ncol(design_mat))
-  sig_xi_inv <- rep(NA, total_samples)
+  sig_xi <- rep(NA, total_samples)
   sep_eta_pred <- matrix(NA, nrow = nrow(schoolsM), ncol = total_samples)
   random_slope_pred <- matrix(NA, nrow = nrow(schoolsM), ncol = total_samples)
   # Bayesian - Random Slope Model ----------------------------------------------------------------------------------------  
@@ -112,15 +100,16 @@ for(years in years_to_pred){
   # Block matrix inversion, save computation
   sparse_design <- Matrix(design_here, sparse = TRUE)
   tilde_eta_rs <- matrix(NA, ncol = total_samples, nrow = ncol(design_here))
-  sig_xi_inv <- rep(NA, total_samples)
-  sig_eta_inv <- rep(NA, total_samples)
-  curr_eta <- matrix(1,nrow = dim(design_here)[2], ncol = 1)
-  curr_sig_xi_inv <- .1
-  curr_sig_eta_inv <- .01
-  curr_omega <- rep(1, nrow(schoolsM))
+  sig_xi <- rep(NA, total_samples)
+  sig_eta <- rep(NA, total_samples)
+  rr <- rep(NA, total_samples)
+  curr_eta <- matrix(0,nrow = dim(design_here)[2], ncol = 1)
+  curr_sig_xi <- .1
+  curr_sig_eta <- .1
+  curr_omega <- rep(1, length(Yin))
 
   prior_mu_eta <- rep(0, dim(design_here)[2])
-  curr_r = 20
+  curr_r = 10
   curr_idx <- 1
   save_idx <- 0
   
@@ -129,51 +118,70 @@ for(years in years_to_pred){
     b_it = as.vector(Yin) + curr_r
     kappa_it = curr_r - b_it/2
     curr_psi = sparse_design %*% curr_eta
-    curr_omega = pgdraw(b = b_it, c = as.matrix(curr_psi))
-    # Sample current eta_tilde
-    curr_B <- Matrix(diag(c(rep(1/curr_sig_eta_inv^2, nh*ns), rep(1/curr_sig_xi_inv^2, ns))),sparse = TRUE)
+    curr_omega = pgdraw(b = b_it, c = as.vector(curr_psi))
+    
+    # Sample current eta
+    curr_B <- Matrix(diag(c(rep(curr_sig_eta, nh*ns), rep(curr_sig_xi, ns))),sparse = TRUE)
     pos_sigma_eta <- solve(t(sparse_design)%*%(sparse_design * curr_omega)  + solve(curr_B))
     pos_mu_eta <- pos_sigma_eta%*%(t(sparse_design)%*%kappa_it)
     
     L <- chol(pos_sigma_eta)
     sth <- rnorm(length(pos_mu_eta))
-    curr_eta <- L %*% sth + pos_mu_eta
-    # curr_eta <- mvtnorm::rmvnorm(1, mean = pos_mu_eta, sigma = pos_sigma_eta)
-    
-    # Propose a sigma_xi
-    d <- min(0.5, 1/curr_sig_xi_inv)
-    temp_sig_xi_inv <- 1/runif(1, min = 1/curr_sig_xi_inv-d, max = 1/curr_sig_xi_inv+d)
-    # temp_sig_xi_inv <- exp(rnorm(1,mean = log(curr_sig_xi_inv), sd = 1))
-    # Metropolis-hastings
-    prev_loglike <- pos_sig_xi(sig_xi =  1/curr_sig_xi_inv, xi = curr_eta[(length(curr_eta)-ns+1):length(curr_eta)])
-    curr_loglike <- pos_sig_xi(sig_xi = 1/temp_sig_xi_inv, xi = curr_eta[(length(curr_eta)-ns+1):length(curr_eta)])
-    p_trans <- exp(curr_loglike - prev_loglike)
-    l <- runif(1)
-    if(p_trans > l){
-      curr_sig_xi_inv <- temp_sig_xi_inv
-    }
+    curr_eta <- t(L) %*% sth + pos_mu_eta
+    # curr_eta <- as.vector(mvtnorm::rmvnorm(1, mean = pos_mu_eta, sigma = as.matrix(pos_sigma_eta)))
     
     # Propose a sigma_eta
-    d <- min(1, 1/curr_sig_eta_inv)
-    temp_sig_eta_inv <- 1/runif(1, min = 1/curr_sig_eta_inv-d, max = 1/curr_sig_eta_inv+d)
-    # temp_sig_eta_inv <- exp(rnorm(1,mean = log(curr_sig_eta_inv), sd = 1))
-    # Metropolis-hastings
-    prev_loglike <- pos_sig_eta(sig_eta =  1/curr_sig_eta_inv, eta_trunc = curr_eta[1:(ns*nh)])
-    curr_loglike <- pos_sig_eta(sig_eta = 1/temp_sig_eta_inv, eta_trunc = curr_eta[1:(ns*nh)])
-    p_trans <- exp(curr_loglike - prev_loglike)
-    l <- runif(1)
-    if(p_trans > l){
-      curr_sig_eta_inv <- temp_sig_eta_inv
-    }
+    alpha_eta_pos = ns*nh/2+alpha_eta
+    beta_eta_pos = sum(curr_eta[1:(nh*ns)]^2/2)+beta_eta
+    curr_sig_eta = 1/rgamma(1, shape = alpha_eta_pos,rate = beta_eta_pos)
+    
+    
+    # Propose a sigma_xi
+    alpha_xi_pos = ns/2+alpha_xi
+    beta_xi_pos = sum(curr_eta[(nh*ns+1):length(curr_eta)]^2/2)+beta_xi
+    curr_sig_xi = 1/rgamma(1, shape = alpha_xi_pos,rate = beta_xi_pos)
+    
+    
+    # Propose an r
+    # Prior: discrete uniform distribution [1:20]
+    # if(curr_r == 1 ){
+    #   new_r <- 2
+    #   curr_llh <- sum(lgamma(as.vector(Yin+curr_r))) - length(as.vector(Yin))*lgamma(curr_r) 
+    #   + curr_r*sum(sparse_design%*%curr_eta) - curr_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   new_llh <- sum(lgamma(as.vector(Yin+new_r))) - length(as.vector(Yin))*lgamma(new_r)
+    #   + new_r*sum(sparse_design%*%curr_eta) - new_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   
+    #   p_trans <- exp(new_llh - curr_llh) * 0.5
+    # }else if(curr_r == 20 ){
+    #   new_r <- 19
+    #   curr_llh <- sum(lgamma(as.vector(Yin+curr_r))) - length(as.vector(Yin))*lgamma(curr_r)
+    #   + curr_r*sum(sparse_design%*%curr_eta) - curr_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   
+    #   new_llh <- sum(lgamma(as.vector(Yin+new_r))) - length(as.vector(Yin))*lgamma(new_r)
+    #   + new_r*sum(sparse_design%*%curr_eta) - new_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   p_trans <- exp(new_llh - curr_llh) * 0.5
+    # }else{
+    #   walker = runif(1)
+    #   new_r = ifelse(walker>0.5, curr_r+1,curr_r-1)
+    #   curr_llh <- sum(lgamma(as.vector(Yin+curr_r))) - length(as.vector(Yin))*lgamma(curr_r)
+    #   + curr_r*sum(sparse_design%*%curr_eta) - curr_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   
+    #   new_llh <- sum(lgamma(as.vector(Yin+new_r))) - length(as.vector(Yin))*lgamma(new_r)
+    #   + new_r*sum(sparse_design%*%curr_eta) - new_r*sum(log(1+exp(sparse_design%*%curr_eta)))
+    #   p_trans <- exp(new_llh - curr_llh)
+    # }
+    # 
+    # l = runif(1)
+    # curr_r = ifelse(l < p_trans, new_r, curr_r)
     
     curr_idx <- curr_idx + 1 
     # Save current values after burn and thin
     if( (curr_idx > burn) & ((curr_idx - burn)%%thin == 0) ){
       save_idx <- save_idx + 1
       tilde_eta_rs[,save_idx] <- as.vector(curr_eta)
-      sig_xi_inv[save_idx] <- curr_sig_xi_inv
-      sig_eta_inv[save_idx] <- curr_sig_eta_inv
-      
+      sig_xi[save_idx] <- curr_sig_xi
+      sig_eta[save_idx] <- curr_sig_eta
+      rr[save_idx] <- curr_r
       
       # Initialize the transformed matrix
       transformed_H_pred <- matrix(NA, nrow = nrow(H$pred_h), ncol = nh * ns)
@@ -186,11 +194,13 @@ for(years in years_to_pred){
       random_slope_pred[,save_idx] <- curr_r * (1-curr_p)/curr_p
       # pred_all_randslp[years-min(years_to_pred)+1,,] <- random_slope_pred
       
-      par(mfrow = c(4,1), mar = c(2,2,2,2))
-      plot(x = 1:save_idx, y = sig_xi_inv[1:save_idx], type = 'l', main = "sig xi inv", xlab = "")
-      plot(x = 1:save_idx, y = 1/sig_xi_inv[1:save_idx], type = 'l', main = "sig xi", xlab = "")
-      plot(x = 1:save_idx, y = sig_eta_inv[1:save_idx], type = 'l', main = "sig eta inv", xlab = "")
-      plot(x = 1:save_idx, y = 1/sig_eta_inv[1:save_idx], type = 'l', main = "sig eta", xlab = "")
+      par(mfrow = c(5,1), mar = c(2,2,2,2))
+      # plot(x = 1:save_idx, y = sig_xi_inv[1:save_idx], type = 'l', main = "sig xi inv", xlab = "")
+      plot(x = 1:save_idx, y = sig_xi[1:save_idx], type = 'l', main = "sig xi", xlab = "")
+      # plot(x = 1:save_idx, y = sig_eta_inv[1:save_idx], type = 'l', main = "sig eta inv", xlab = "")
+      plot(x = 1:save_idx, y = sig_eta[1:save_idx], type = 'l', main = "sig eta", xlab = "")
+      plot(x = 1:save_idx, y = rr[1:save_idx], type = 'l', main = "r", xlab = "")
+      
     } 
     pred <- random_slope_pred[,save_idx]
     true_value <- schoolsM[,years]
